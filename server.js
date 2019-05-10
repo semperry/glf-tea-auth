c6d53240978998b2a2be0979bae8a99daf8ac9a7
semperry <ryan.curtis88@gmail.com>
2019-05-10 14:06:18 -0600

    testing cors policy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const cors = require('cors')
 
 const users = require("./routes/api/users");
 
@@ -14,6 +15,7 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+app.use(cors())
 
 // DB Config
 const db = require("./config/keys").mongoURI;
